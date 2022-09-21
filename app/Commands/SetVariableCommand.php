5f0e674cfe870e0d3220f4d4e482d56a68f3bc67
Rajesh Chaudhary <rajeshisnepali@gmail.com>
2022-09-21 10:08:32 +0545

    chore: set dev env as not protected

--- app/Commands/SetVariableCommand.php
+++ app/Commands/SetVariableCommand.php
@@ -11,7 +11,7 @@ use function Termwind\{render};
 class SetVariableCommand extends Command
 {
 
-    const SET_DEV_PROTECTED = true;
+    const SET_DEV_PROTECTED = false;
     const SET_STAGING_PROTECTED = true;
     /**
      * The signature of the command.
